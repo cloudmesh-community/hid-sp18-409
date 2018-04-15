2b6821ba44a4b38f7d439ee204789270efe19deb
Gregor von Laszewski <laszewski@gmail.com>
2018-04-15 01:05:36 -0400

    better readability

--- swagger/cloudmesh/crime_finder/data/testClient.py
+++ swagger/cloudmesh/crime_finder/data/testClient.py
@@ -9,10 +9,14 @@ from __future__ import print_function
 import swagger_client
 from swagger_client.rest import ApiException
 from pprint import pprint
+######################################################################
 # create an instance of the API class
+######################################################################
 api_instance = swagger_client.CrimesApi()
 
-    #test case 1: Api:  /crimes
+######################################################################
+#test case 1: Api:  /crimes
+######################################################################
 print('test case 1: Api:  /crimes')
 try:
     latitude=41.891398861
@@ -22,8 +26,9 @@ try:
 except ApiException as e:
     print("Exception occured calling DefaultApi->cpu_get: %s\n" % e)
     
-    
-    #test case 2: Api:  /crimes/search
+######################################################################    
+#test case 2: Api:  /crimes/search
+######################################################################
 print('test case 2: Api:  /crimes/search')
 try:
     crime_id='10007143'
@@ -32,8 +37,9 @@ try:
 except ApiException as e:
     print("Exception occured calling DefaultApi->cpu_get: %s\n" % e)
 
-    
-    #test case 3: Api:  /crimes/filter 
+######################################################################
+#test case 3: Api:  /crimes/filter 
+######################################################################
 print('test case 3: Api:  /crimes/filter')
 try:
     latitude=41.891398861
